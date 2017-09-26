4b5142180bb18a22c9088cbb2c088eee7e3546ce
Catherine Ta <catherineta@Catherines-MacBook-Air.local>
2017-09-26 19:36:52 -0400

    Can access text in tweet-box but need to make event handler

--- content.js
+++ content.js
@@ -1,2 +1,10 @@
 // content.js
-alert("test")
+
+
+// Gets the text in the tweet-box if there is existing text
+// --> Need to figure out how to make event handlers for Chrome extensions
+setTimeout(function() { 
+    if (document.querySelector("#tweet-box-home-timeline") && document.querySelector("#tweet-box-home-timeline").querySelector("div")) {
+        console.log(document.querySelector("#tweet-box-home-timeline").querySelector("div").innerText);
+    }
+}, 0);
