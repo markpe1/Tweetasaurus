221d14ef5d58550f6ef9ea092b1c47744223805f
Mark Episcopo <markpe1@vt.edu>
2017-10-03 14:00:57 -0400

    updates the console log with key presses on the post block

---
+++ background.js
@@ -0,0 +1,7 @@
+// background.js
+
+// Called when the user clicks on the browser action.
+
+  addEventListener("keypress", function(event) {
+    console.log(String.fromCharCode(event.charCode));
+  });
--- content.js
+++ content.js
@@ -8,3 +8,34 @@ setTimeout(function() {
         console.log(document.querySelector("#tweet-box-home-timeline").querySelector("div").innerText);
     }
 }, 0);
+
+  addEventListener("keypress", function(event) {
+  	if (document.querySelector("#tweet-box-home-timeline") && document.querySelector("#tweet-box-home-timeline").querySelector("div")) {
+        var textBox = document.querySelector("#tweet-box-home-timeline").querySelector("div").innerText
+        console.log(textBox)
+	}
+  	function abbreviate(string) {
+	  var words = string.split(" ");
+	  for (var i = 0; i < words.length; i += 1) {
+
+	  var count = words[i].length - 2;
+	  var last = words[i].charAt(words[i].length - 1);
+	  answer= answer + words[i][0] + count + last;
+	  }
+	return answer;
+	}
+    console.log(String.fromCharCode(event.charCode));
+  });
+
+// chrome.runtime.onMessage.addListener(
+//   function(request, sender, sendResponse) {
+//     if( request.message === "clicked_browser_action" ) {
+//       var firstHref = $("a[href^='http']").eq(0).attr("href");
+
+//       console.log(firstHref);
+
+//       // This line is new!
+//       chrome.runtime.sendMessage({"message": "open_new_tab", "url": firstHref});
+//     }
+//   }
+// );
