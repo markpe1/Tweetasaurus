e107e887a3436c5f9e1dc21bd1dc15cff7d13212
Catherine Ta <catherineta@Catherines-MacBook-Air.local>
2017-10-03 14:40:41 -0400

    Add context menu on right click

--- background.js
+++
@@ -1,7 +0,0 @@
-// background.js
-
-// Called when the user clicks on the browser action.
-
-  addEventListener("keypress", function(event) {
-    console.log(String.fromCharCode(event.charCode));
-  });
