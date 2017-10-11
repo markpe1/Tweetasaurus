e3cef6c1c81882cdf3234c32762530d5c68c13b6
Catherine Ta <catherineta@Catherines-MacBook-Air.local>
2017-10-11 11:33:19 -0400

    sig

--- content.js
+++ content.js
@@ -1,24 +1,30 @@
-// content.js
+/* content.js */
 
 alert("You are on Twitter");
-    
-
-// content.js
+	
+// Event listener for getting text from tweet
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
-    if( request.message === "clicked_browser_action" ) {
-        var elem = document.querySelector("#tweet-box-home-timeline").querySelector("div")
-        var textBox = elem.outerHTML
-        var style = textBox
-        var words = ["idiot", "dummy", "jerk"]
-        for (var i = 0; i < words.length; i++)
-        {
+    if (request.message === "clicked_browser_action") {
+
+		// Select the div that includes the text
+        var elem = document.querySelector("#tweet-box-home-timeline").querySelector("div");
+        var textBox = elem.outerHTML;
+		var style = textBox;
+		
+		// Array for negative test words
+		var words = ["idiot", "dummy", "jerk"];
+		
+		// Change the color of the text for negative words
+        for (var i = 0; i < words.length; i++) {
         	style = style.replace(new RegExp(words[i], 'g'), '<span style="color:red ! important;">' + words[i] + '</span>');
         }
 	    
 	    textBox = style;
-	    console.log(textBox)
-	    elem.outerHTML = textBox
+		console.log(textBox);
+		
+		// Update the style
+	    elem.outerHTML = textBox;
     }
   }
 );
