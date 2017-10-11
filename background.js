535ad74728ab5ffd29959cd7fff39124814db5b4
Catherine Ta <catherineta@Catherines-MacBook-Air.local>
2017-10-11 11:41:45 -0400

    add one line lol

--- background.js
+++ background.js
@@ -2,6 +2,7 @@
 
 // Called when the user clicks on the browser action.
 chrome.browserAction.onClicked.addListener(function(tab) {
+  
   // Send a message to the active tab
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     var activeTab = tabs[0];
