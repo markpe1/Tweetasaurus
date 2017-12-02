a6673760be456fe4ce143750839c578f609d58a2
Catherine Ta <catherineta@Catherines-MacBook-Air.local>
2017-12-02 15:37:46 -0500

    update lowercase

--- content.js
+++ content.js
@@ -1,17 +1,17 @@
 /* content.js */
 
-var head = document.getElementsByTagName('head')[0];
-var script = document.createElement('script');
-script.type = 'text/javascript';
-script.src = "https://apis.google.com/js/client.js?onload=callbackFunction";
-head.appendChild(script);
+// var head = document.getElementsByTagName('head')[0];
+// var script = document.createElement('script');
+// script.type = 'text/javascript';
+// script.src = "https://apis.google.com/js/client.js?onload=callbackFunction";
+// head.appendChild(script);
 
 var count = 0; // Count for number of offensive words detected in the tweet
 
 // Dictionary for negative words
 var dictionary = ["fagot", "fuckwit", "cocksucker", "dumbass", "asshat", "shithead", "prick", "horseshit", "dogshit", "shitty", "apeshit", "arsehole", "asshole", "bastard", "bitch", "bollocks", "bullshit", "bunghole", "butthole",
-"cock", "cunt", "dick", "dickhead", "faggot", "fucker", "fucking", "goddamn", "jackass", "motherfucker", "penis", "pussy", 
-"damn", "shit", "slut", "puss", "fuck", "ass", "whore", "fag"];
+"cock", "cunt", "dickhead", "faggot", "fucker", "fucking", "goddamn", "jackass", "motherfucker", "penis", "pussy", "fucker",
+"damn", "shit", "slut", "puss", "fuck", "ass", "whore", "fag", "dick", "cock"];
 
 // // Display for toxicity level
 // var tox = '<div id="tox">Toxicity Level: <span id="level">0</span></div>';
@@ -58,19 +58,20 @@ chrome.runtime.onMessage.addListener(
 		var addedWords = [];
 		var contains = false;
 		var elem = document.querySelector("#tweet-box-home-timeline").querySelector("div");
-		var textBox = elem.outerHTML;
-		var style = textBox;
+		var orig = elem.outerHTML;
+		var textBox = orig.toLowerCase();
+		var style = orig;
 			
 		// Change the color of the text for negative words
 		for (var i = 0; i < dictionary.length; i++) {
 			if (textBox.indexOf(dictionary[i]) !== -1) {
 				for (var j = 0; j < addedWords.length; j++) {
 					if (addedWords[j].indexOf(dictionary[i]) !== -1) {
-					contains = true;
+						contains = true;
 					}
 				}
-				if (contains == false) {
-					var newstyle = style.replace(new RegExp(dictionary[i], 'g'), '<span style="color:red ! important;">' + dictionary[i] + '</span>');
+				if (!contains) {
+					var newstyle = style.replace(new RegExp(preg_quote( dictionary[i] ), 'gi' ), '<span style="color:red ! important;">' + dictionary[i] + '</span>');
 					style = newstyle;
 					chrome.runtime.sendMessage({"message": dictionary[i]});
 					addedWords.push(dictionary[i])
@@ -80,8 +81,7 @@ chrome.runtime.onMessage.addListener(
 		}
 
 		// Update the style and the tweet box
-		textBox = style;
-		elem.outerHTML = textBox;
+		elem.outerHTML = style;
     }
     else {
         var elem = document.querySelector("#tweet-box-home-timeline").querySelector("div");
@@ -101,8 +101,8 @@ addEventListener("keyup", function(event) {
   var elem = document.querySelector("#tweet-box-home-timeline").querySelector("div");
   var addedWords = [];
   var contains = false;
-	var textBox = elem.outerHTML;
-	var style = textBox;
+  var textBox = elem.outerHTML;
+  var style = textBox;
 	
 	// Change the color of the text for negative words
 	for (var i = 0; i < dictionary.length; i++) {
@@ -112,7 +112,7 @@ addEventListener("keyup", function(event) {
       		}
     	}
 		if (contains == false) {
-			var newstyle = style.replace(new RegExp(dictionary[i], 'g'), '<span style="color:red ! important;">' + dictionary[i] + '</span>');
+			var newstyle = style.replace(new RegExp(preg_quote( dictionary[i] ), 'gi' ), '<span style="color:red ! important;">' + dictionary[i] + '</span>');
 			if (!(newstyle === style)) {
 				count++;
 				addedWords.push(dictionary[i])
@@ -124,3 +124,18 @@ addEventListener("keyup", function(event) {
 	count = 0;
 })
 
+function preg_quote( str ) {
+    // http://kevin.vanzonneveld.net
+    // +   original by: booeyOH
+    // +   improved by: Ates Goral (http://magnetiq.com)
+    // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+    // +   bugfixed by: Onno Marsman
+    // *     example 1: preg_quote("$40");
+    // *     returns 1: '\$40'
+    // *     example 2: preg_quote("*RRRING* Hello?");
+    // *     returns 2: '\*RRRING\* Hello\?'
+    // *     example 3: preg_quote("\\.+*?[^]$(){}=!<>|:");
+    // *     returns 3: '\\\.\+\*\?\[\^\]\$\(\)\{\}\=\!\<\>\|\:'
+
+    return (str+'').replace(/([\\\.\+\*\?\[\^\]\$\(\)\{\}\=\!\<\>\|\:])/g, "\\$1");
+}
